f1eb36ef493387ce9389fd427defb31e88574b63
Fares <hsnfars20@gmail.com>
2025-01-12 17:55:56 +0200

    add simple blocobserver

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:notely/constants.dart';
 import 'package:notely/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notely/models/note_model.dart';
+import 'package:notely/simple_bloc_observer.dart';
 import 'package:notely/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Bloc.observer = SimpleBlocObserver();
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox(kNotes);
   runApp(const Notely());
@@ -23,9 +25,16 @@ class Notely extends StatelessWidget {
         BlocProvider(
           create: (context) => AddNoteCubit(),
         ),
-       
       ],
-      child: Container(),
+      child: MaterialApp(
+        title: "Notely",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home: const NotesView(),
+      ),
     );
   }
 }
